17bc6e507a7920add5963ca9236fa73686838c24
Lucas Kuligowski <lucaskuligowski@gmail.com>
2024-05-16 18:39:08 -0300

    Delete Tcc directory

--- Tcc/Scripts/CutscenesScripts/InicialCutscene.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-
-func _process(delta):
-	
-	if Input.is_action_just_pressed("ui_accept"):
-		SceneTransition.change_scene("res://Scenes/PlayableScenes/Main.tscn")
-	
--- Tcc/Scripts/DialoguesScripts/InicialDialogue.gd
+++
@@ -1,8 +0,0 @@
-extends NinePatchRect
-
-var dialogue_started = false
-
-func _process(delta):
-	if !dialogue_started:
-		$Text.start_dialogue("Luka")
-		dialogue_started = true
